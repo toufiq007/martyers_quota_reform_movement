f9e967d458eeb36f06401334849118fe88f2e2ca
toufiq007 <t.limon2342@gmail.com>
2024-08-04 00:00:02 +0600

    change in schema,controller and model

--- backend/src/model/people.model.ts
+++ backend/src/model/people.model.ts
@@ -4,15 +4,15 @@ import { IPeople } from "../types/peopleSchemaType";
 // Define the Mongoose schema
 const peopleSchema: Schema = new Schema(
   {
-    name: { type: String, required: true },
-    age: { type: Number, required: true },
-    address: { type: String, required: true },
-    birthOfDate: { type: String, required: true },
-    deathOfDate: { type: String, required: false },
-    institution: { type: String, required: true },
-    causeOfDeath: { type: String, required: false },
-    history: { type: String, required: true },
-    personalImage: { type: String, required: true },
+    name: { type: String },
+    age: { type: Number },
+    address: { type: String },
+    birthOfDate: { type: String },
+    died: { type: String },
+    institution: { type: String },
+    causeOfDeath: { type: String },
+    history: { type: String },
+    personalImage: { type: String },
   },
   { timestamps: true }
 );
--- backend/src/types/peopleSchemaType.ts
+++ backend/src/types/peopleSchemaType.ts
@@ -1,10 +1,10 @@
 export interface IPeople extends Document {
-  name: string;
-  age: number;
-  address: string;
-  birthOfDate: string;
-  deathOfDate: string;
-  institution: string;
-  causeOfDeath: string;
-  history: string;
+  name?: string;
+  age?: number;
+  address?: string;
+  birthOfDate?: string;
+  deathOfDate?: string;
+  institution?: string;
+  causeOfDeath?: string;
+  history?: string;
 }
