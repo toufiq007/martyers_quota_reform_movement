1d023272ff323ac71a86005d4739fe0ca6e8bf3f
toufiq007 <t.limon2342@gmail.com>
2024-08-04 02:58:24 +0600

    create a loader component and use it when aysnchrous call

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -2,25 +2,52 @@
 import { useEffect, useState } from "react";
 import Card from "../../components/card";
 import "./style.css";
+import Loader from "../../components/loader";
 
 const HomePage = () => {
   const [martyers, setMartyers] = useState([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
   useEffect(() => {
+    setIsLoading(true);
     fetch(`http://localhost:5000/api/getAllMatryers`)
-      .then((res) => res.json())
-      .then((data) => setMartyers(data));
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setMartyers(data);
+        setIsSuccess(true);
+        setIsError(false);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data:", error);
+        setIsError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
   console.log(martyers, "from the home page");
   return (
     <div className="home_page">
-      <h1 className="banner_heading">List of Martys in the Quota Reform Movement </h1>
-      <div className="card_container">
-        {martyers?.user &&
-          martyers?.user?.length > 0 &&
-          martyers?.user?.map((singleUser: any, index: number) => (
-            <Card singleUser={singleUser} index={index} />
-          ))}
-      </div>
+      <h1 className="banner_heading">
+        List of Martys in the Quota Reform Movement{" "}
+      </h1>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="card_container">
+          {martyers?.user &&
+            martyers?.user?.length > 0 &&
+            martyers?.user?.map((singleUser: any, index: number) => (
+              <Card singleUser={singleUser} index={index} />
+            ))}
+        </div>
+      )}
     </div>
   );
 };
--- client/src/pages/martyerDetails/index.tsx
+++ client/src/pages/martyerDetails/index.tsx
@@ -1,16 +1,36 @@
 import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Loader from "../../components/loader";
 
 const MartyerDetails = () => {
   const { id } = useParams();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
   console.log(id);
   const [martyer, setMartyer] = useState(null);
   const navigate = useNavigate();
   useEffect(() => {
+    setIsLoading(true);
     fetch(`http://localhost:5000/api/getMartyer/${id}`)
-      .then((res) => res.json())
-      .then((data) => setMartyer(data));
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setMartyer(data);
+        setIsError(false);
+        setIsSuccess(false);
+      })
+      .catch((err) => {
+        setIsError(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
   const handleGoBack = () => {
     navigate("/");
@@ -19,102 +39,114 @@ const MartyerDetails = () => {
   console.log(martyer?.people[0]?.name);
 
   return (
-    <div>
-      <h1 className="martyers_name">{martyer?.people[0]?.name}</h1>
-      <div
-        className="martyers_container"
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(2,1fr)",
-          width: "90%",
-          margin: "auto",
-        }}
-      >
-        <div
-          className="martyers_image"
-          style={{ display: "flex", flexDirection: "column", width: "600px" }}
-        >
-          <img
-            style={{ width: "100%", height:"500px",objectFit: "cover" }}
-            src={`${martyer?.people[0]?.personalImage}`}
-          />
-          <div>
-            <Button
-              variant="contained"
-              sx={{ margin: "10px 0" }}
-              onClick={handleGoBack}
-            >
-              Go Back
-            </Button>
-          </div>
-        </div>
-        <div className="martyers_info" style={{ width: "600px" }}>
-          <div className="personal_info" style={{ background: "#ededed" }}>
-            <p>Profession: </p>
-            <p>Born:</p>
-            <p>Age: </p>
-            <p>Hometown: </p>
-            <p>Died</p>
-            <p>Education</p>
-          </div>
-          <div className="short_biography">
-            <p
-              className="martyers_biography_heading"
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <h1 className="martyers_name">{martyer?.people[0]?.name}</h1>
+          <div
+            className="martyers_container"
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(2,1fr)",
+              width: "90%",
+              margin: "auto",
+            }}
+          >
+            <div
+              className="martyers_image"
               style={{
-                fontSize: "20px",
-                fontWeight: "600",
-                margin: "10px",
-                textAlign: "right",
+                display: "flex",
+                flexDirection: "column",
+                width: "600px",
               }}
             >
-              Short Biography
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              eius quibusdam perspiciatis suscipit illo veniam dolorem
-              praesentium ex accusantium? Architecto, ratione ullam numquam
-              doloribus eaque iure eius nesciunt aliquid, tempore fuga minus
-              minima, sint cum distinctio! Provident optio possimus in vitae
-              beatae, tempora, asperiores aperiam laborum velit numquam dicta
-              non! Libero quam, cum recusandae dolorem eaque culpa magni a fugit
-              nisi, at ipsam nobis architecto. Aliquam aut cupiditate explicabo
-              a nam odit voluptate corporis labore incidunt commodi ipsa,
-              officiis, doloribus quos temporibus? Nulla, odio debitis ab quam
-              eaque nam deleniti quo cumque eos repellendus natus, aspernatur
-              recusandae ut neque qui!
-            </p>
-          </div>
-          <div className="martyers_death">
-            <p
-              className="martyers_death_heading"
-              style={{
-                fontSize: "20px",
-                fontWeight: "600",
-                margin: "10px",
-                textAlign: "right",
-              }}
-            >
-              Death
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-              animi earum voluptas exercitationem dicta! Deserunt suscipit
-              dolorem aspernatur explicabo quasi animi distinctio. Ipsa quidem
-              molestias dolores? Nam aut quam reprehenderit, debitis dolorem
-              laboriosam nulla id aspernatur consequatur dolores. Perspiciatis
-              est minima excepturi fugit quibusdam enim maiores facilis nemo
-              quisquam necessitatibus dolor, esse, expedita optio officia
-              voluptas corrupti! Architecto ipsam quam beatae omnis provident.
-              Quidem impedit ut optio cupiditate saepe corporis eum assumenda
-              voluptate. Dignissimos minima beatae laudantium error eveniet odio
-              hic odit! Tenetur nesciunt ducimus alias, necessitatibus optio
-              suscipit id delectus dolore reprehenderit, magni quidem laborum
-              autem dignissimos et maxime.
-            </p>
+              <img
+                style={{ width: "100%", height: "500px", objectFit: "cover" }}
+                src={`${martyer?.people[0]?.personalImage}`}
+              />
+              <div>
+                <Button
+                  variant="contained"
+                  sx={{ margin: "10px 0" }}
+                  onClick={handleGoBack}
+                >
+                  Go Back
+                </Button>
+              </div>
+            </div>
+            <div className="martyers_info" style={{ width: "600px" }}>
+              <div className="personal_info" style={{ background: "#ededed" }}>
+                <p>Profession: </p>
+                <p>Born:</p>
+                <p>Age: </p>
+                <p>Hometown: </p>
+                <p>Died</p>
+                <p>Education</p>
+              </div>
+              <div className="short_biography">
+                <p
+                  className="martyers_biography_heading"
+                  style={{
+                    fontSize: "20px",
+                    fontWeight: "600",
+                    margin: "10px",
+                    textAlign: "right",
+                  }}
+                >
+                  Short Biography
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ducimus eius quibusdam perspiciatis suscipit illo veniam
+                  dolorem praesentium ex accusantium? Architecto, ratione ullam
+                  numquam doloribus eaque iure eius nesciunt aliquid, tempore
+                  fuga minus minima, sint cum distinctio! Provident optio
+                  possimus in vitae beatae, tempora, asperiores aperiam laborum
+                  velit numquam dicta non! Libero quam, cum recusandae dolorem
+                  eaque culpa magni a fugit nisi, at ipsam nobis architecto.
+                  Aliquam aut cupiditate explicabo a nam odit voluptate corporis
+                  labore incidunt commodi ipsa, officiis, doloribus quos
+                  temporibus? Nulla, odio debitis ab quam eaque nam deleniti quo
+                  cumque eos repellendus natus, aspernatur recusandae ut neque
+                  qui!
+                </p>
+              </div>
+              <div className="martyers_death">
+                <p
+                  className="martyers_death_heading"
+                  style={{
+                    fontSize: "20px",
+                    fontWeight: "600",
+                    margin: "10px",
+                    textAlign: "right",
+                  }}
+                >
+                  Death
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Veritatis animi earum voluptas exercitationem dicta! Deserunt
+                  suscipit dolorem aspernatur explicabo quasi animi distinctio.
+                  Ipsa quidem molestias dolores? Nam aut quam reprehenderit,
+                  debitis dolorem laboriosam nulla id aspernatur consequatur
+                  dolores. Perspiciatis est minima excepturi fugit quibusdam
+                  enim maiores facilis nemo quisquam necessitatibus dolor, esse,
+                  expedita optio officia voluptas corrupti! Architecto ipsam
+                  quam beatae omnis provident. Quidem impedit ut optio
+                  cupiditate saepe corporis eum assumenda voluptate. Dignissimos
+                  minima beatae laudantium error eveniet odio hic odit! Tenetur
+                  nesciunt ducimus alias, necessitatibus optio suscipit id
+                  delectus dolore reprehenderit, magni quidem laborum autem
+                  dignissimos et maxime.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
