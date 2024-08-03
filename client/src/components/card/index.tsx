293b0b0b5143ea688f218c4027672b8f0ac899b2
toufiq007 <t.limon2342@gmail.com>
2024-08-04 00:07:50 +0600

    successfully fetch a singleMartyer by using id

--- client/src/components/card/index.tsx
+++ client/src/components/card/index.tsx
@@ -1,15 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import "./style.css"
+import { useNavigate } from "react-router-dom";
+import "./style.css";
 
 const Card = ({ singleUser, index }: any) => {
+  const navigate = useNavigate();
+  const handleMartyerDetails = (id: string) => {
+    navigate(`/martyer/${id}`);
+  };
   return (
     <div
-    className="martyers_card"
+      className="martyers_card"
       key={index}
+      onClick={() => handleMartyerDetails(singleUser?._id)}
     >
-      <img
-        src={`${singleUser?.personalImage}`}
-      />
+      <img src={`${singleUser?.personalImage}`} />
       <p className="martyers_name">{singleUser?.name}</p>
       <p className="martyers_institution">{singleUser?.institution}</p>
       <span className="martyers_died">18th July, 2024</span>
--- client/src/pages/martyerDetails/index.tsx
+++ client/src/pages/martyerDetails/index.tsx
@@ -1,14 +1,23 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const MartyerDetails = () => {
+  const { id } = useParams();
+  console.log(id);
+  const [martyer, setMartyer] = useState(null);
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/getMartyer/${id}`)
+      .then((res) => res.json())
+      .then((data) => setMartyer(data));
+  }, []);
 
-    const {id} = useParams()
-    console.log(id)
-    return (
-        <div>
-            <h2>Details information</h2>
-        </div>
-    );
+  console.log(martyer);
+
+  return (
+    <div>
+      <h2>Details information</h2>
+    </div>
+  );
 };
 
 export default MartyerDetails;
