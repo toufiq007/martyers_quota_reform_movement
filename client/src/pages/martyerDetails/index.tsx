51bcd6161b61ea98c5d564f2ff6758f9045897db
toufiq007 <t.limon2342@gmail.com>
2024-08-04 02:08:51 +0600

    structured martyerDetails page and add data

--- client/src/pages/martyerDetails/index.tsx
+++ client/src/pages/martyerDetails/index.tsx
@@ -1,21 +1,119 @@
+import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const MartyerDetails = () => {
   const { id } = useParams();
   console.log(id);
   const [martyer, setMartyer] = useState(null);
+  const navigate = useNavigate();
   useEffect(() => {
     fetch(`http://localhost:5000/api/getMartyer/${id}`)
       .then((res) => res.json())
       .then((data) => setMartyer(data));
   }, []);
+  const handleGoBack = () => {
+    navigate("/");
+  };
 
-  console.log(martyer);
+  console.log(martyer?.people[0]?.name);
 
   return (
     <div>
-      <h2>Details information</h2>
+      <h1 className="martyers_name">{martyer?.people[0]?.name}</h1>
+      <div
+        className="martyers_container"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2,1fr)",
+          width: "90%",
+          margin: "auto",
+        }}
+      >
+        <div
+          className="martyers_image"
+          style={{ display: "flex", flexDirection: "column", width: "600px" }}
+        >
+          <img
+            style={{ width: "100%", height:"500px",objectFit: "cover" }}
+            src={`${martyer?.people[0]?.personalImage}`}
+          />
+          <div>
+            <Button
+              variant="contained"
+              sx={{ margin: "10px 0" }}
+              onClick={handleGoBack}
+            >
+              Go Back
+            </Button>
+          </div>
+        </div>
+        <div className="martyers_info" style={{ width: "600px" }}>
+          <div className="personal_info" style={{ background: "#ededed" }}>
+            <p>Profession: </p>
+            <p>Born:</p>
+            <p>Age: </p>
+            <p>Hometown: </p>
+            <p>Died</p>
+            <p>Education</p>
+          </div>
+          <div className="short_biography">
+            <p
+              className="martyers_biography_heading"
+              style={{
+                fontSize: "20px",
+                fontWeight: "600",
+                margin: "10px",
+                textAlign: "right",
+              }}
+            >
+              Short Biography
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+              eius quibusdam perspiciatis suscipit illo veniam dolorem
+              praesentium ex accusantium? Architecto, ratione ullam numquam
+              doloribus eaque iure eius nesciunt aliquid, tempore fuga minus
+              minima, sint cum distinctio! Provident optio possimus in vitae
+              beatae, tempora, asperiores aperiam laborum velit numquam dicta
+              non! Libero quam, cum recusandae dolorem eaque culpa magni a fugit
+              nisi, at ipsam nobis architecto. Aliquam aut cupiditate explicabo
+              a nam odit voluptate corporis labore incidunt commodi ipsa,
+              officiis, doloribus quos temporibus? Nulla, odio debitis ab quam
+              eaque nam deleniti quo cumque eos repellendus natus, aspernatur
+              recusandae ut neque qui!
+            </p>
+          </div>
+          <div className="martyers_death">
+            <p
+              className="martyers_death_heading"
+              style={{
+                fontSize: "20px",
+                fontWeight: "600",
+                margin: "10px",
+                textAlign: "right",
+              }}
+            >
+              Death
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              animi earum voluptas exercitationem dicta! Deserunt suscipit
+              dolorem aspernatur explicabo quasi animi distinctio. Ipsa quidem
+              molestias dolores? Nam aut quam reprehenderit, debitis dolorem
+              laboriosam nulla id aspernatur consequatur dolores. Perspiciatis
+              est minima excepturi fugit quibusdam enim maiores facilis nemo
+              quisquam necessitatibus dolor, esse, expedita optio officia
+              voluptas corrupti! Architecto ipsam quam beatae omnis provident.
+              Quidem impedit ut optio cupiditate saepe corporis eum assumenda
+              voluptate. Dignissimos minima beatae laudantium error eveniet odio
+              hic odit! Tenetur nesciunt ducimus alias, necessitatibus optio
+              suscipit id delectus dolore reprehenderit, magni quidem laborum
+              autem dignissimos et maxime.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
