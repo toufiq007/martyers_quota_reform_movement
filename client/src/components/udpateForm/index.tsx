9251489093739f66b4ef032eae5c8278bb06a896
toufiq007 <t.limon2342@gmail.com>
2024-08-05 01:01:00 +0600

    add post request in frontend when update the data

--- client/src/components/udpateForm/index.tsx
+++ client/src/components/udpateForm/index.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { TextField, Button, Box } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import useMartyersDetails from "../../hooks/useMartyersDetails";
+import { useParams } from "react-router-dom";
+import Loader from "../loader";
 
 type IForm = {
   name: string;
@@ -17,18 +20,36 @@ type IForm = {
 };
 
 const UpdateMartyerForm = () => {
+  const { id } = useParams();
+  const { martyer, isLoading: isMartyerLoading } = useMartyersDetails(id);
   const { register, handleSubmit, reset } = useForm<IForm>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [isError, setIsError] = useState(false);
 
-  
+  // set data to the default values
+  useEffect(() => {
+    if (martyer) {
+      reset({
+        name: martyer.people[0]?.name,
+        age: martyer.people[0]?.age,
+        address: martyer.people[0]?.address,
+        birthOfDate: martyer.people[0]?.birthOfDate,
+        died: martyer.people[0]?.died,
+        institution: martyer.people[0]?.institution,
+        causeOfDeath: martyer.people[0]?.causeOfDeath,
+        history: martyer.people[0]?.history,
+      });
+    }
+  }, [martyer, reset]);
+
+  console.log(martyer?.people[0]?.name, "from then update form ");
 
   const onSubmit = async (data: any) => {
     setIsLoading(true);
     setIsSuccess(false);
     setIsError(false);
-    console.log(data);
+    console.log(data,'from then onSubmit function');
     const formData = new FormData();
 
     // append text files
@@ -46,7 +67,7 @@ const UpdateMartyerForm = () => {
       formData.append("personalImage", data.personalImage[0]);
     }
     try {
-      const response = await fetch("http://localhost:5000/api/addNewMatryers", {
+      const response = await fetch(`http://localhost:5000/api/updateMartyer/${id}`, {
         method: "POST",
         body: formData,
       });
@@ -68,96 +89,103 @@ const UpdateMartyerForm = () => {
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit(onSubmit)}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 2,
-        width: "80%",
-        margin: "0 auto",
-      }}
-      encType="multipart/form-data"
-    >
-      <TextField
-        label="Name"
-        variant="outlined"
-        {...register("name")}
-        size="small"
-        fullWidth
-      />
-      <TextField
-        variant="outlined"
-        type="file"
-        {...register("personalImage")}
-        size="small"
-        fullWidth
-      />
-      <TextField
-        label="Age"
-        type="number"
-        variant="outlined"
-        {...register("age")}
-        size="small"
-        fullWidth
-      />
+    <>
+      {isMartyerLoading ? (
+        <Loader />
+      ) : (
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            width: "80%",
+            margin: "0 auto",
+          }}
+          encType="multipart/form-data"
+        >
+          <TextField
+            label="Name"
+            variant="outlined"
+            {...register("name")}
+            size="small"
+            fullWidth
+          />
+          <TextField
+            variant="outlined"
+            type="file"
+            {...register("personalImage")}
+            size="small"
+            fullWidth
+          />
+          <TextField
+            label="Age"
+            type="number"
+            variant="outlined"
+            {...register("age")}
+            size="small"
+            fullWidth
+          />
 
-      <TextField
-        label="Address"
-        variant="outlined"
-        {...register("address")}
-        size="small"
-        fullWidth
-      />
+          <TextField
+            label="Address"
+            variant="outlined"
+            {...register("address")}
+            size="small"
+            fullWidth
+          />
 
-      <TextField
-        label="Institution"
-        variant="outlined"
-        {...register("institution")}
-        size="small"
-        fullWidth
-      />
+          <TextField
+            label="Institution"
+            variant="outlined"
+            {...register("institution")}
+            size="small"
+            fullWidth
+          />
 
-      <TextField
-        label="Date of birth"
-        variant="outlined"
-        {...register("birthOfDate")}
-        size="small"
-        fullWidth
-      />
+          <TextField
+            label="Date of birth"
+            variant="outlined"
+            {...register("birthOfDate")}
+            size="small"
+            fullWidth
+          />
 
-      <TextField
-        label="Date of death"
-        variant="outlined"
-        {...register("died")}
-        size="small"
-        fullWidth
-      />
+          <TextField
+            label="Date of death"
+            variant="outlined"
+            {...register("died")}
+            size="small"
+            fullWidth
+          />
 
-      <TextField
-        label="Cause Of Death"
-        variant="outlined"
-        {...register("causeOfDeath")}
-        size="small"
-        fullWidth
-      />
+          <TextField
+            label="Cause Of Death"
+            variant="outlined"
+            {...register("causeOfDeath")}
+            size="small"
+            fullWidth
+          />
 
-      <TextField
-        label="History"
-        variant="outlined"
-        multiline
-        rows={4}
-        placeholder="enter your history"
-        {...register("history")}
-        size="small"
-        fullWidth
-      />
+          <TextField
+            label="History"
+            variant="outlined"
+            multiline
+            rows={4}
+            placeholder="enter your history"
+            {...register("history")}
+            size="small"
+            fullWidth
+          />
 
-      <Button variant="contained" type="submit">
-        {isLoading ? "loading..." : "Submit"}
-      </Button>
-    </Box>
+          <Button variant="contained" type="submit">
+            {/* {isLoading ? "loading..." : "Submit"} */}
+            submit
+          </Button>
+        </Box>
+      )}
+    </>
   );
 };
 
